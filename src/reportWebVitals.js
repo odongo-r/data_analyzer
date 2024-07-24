2749c5000af6ee5a6e12e8bbfa376708465e8204
Ronald <125483878+odongo-r@users.noreply.github.com>
2024-07-25 00:56:09 +0300

    Update reportWebVitals.js

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,18 @@
-const reportWebVitals = onPerfEntry => {
+// Function to measure web vitals performance metrics
+const reportWebVitals = (onPerfEntry) => {
+  // Check if the provided callback is a function
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // Dynamically import the 'web-vitals' library
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+      // Call each web vitals function with the provided callback
+      getCLS(onPerfEntry); // Cumulative Layout Shift
+      getFID(onPerfEntry); // First Input Delay
+      getFCP(onPerfEntry); // First Contentful Paint
+      getLCP(onPerfEntry); // Largest Contentful Paint
+      getTTFB(onPerfEntry); // Time to First Byte
     });
   }
 };
 
+// Export the reportWebVitals function as the default export
 export default reportWebVitals;
